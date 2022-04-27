ce45098cc61a9944ed70b8d04354f2cbf6db672b
Luis Steidle <luis.steidle@frachtwerk.de>
2022-04-27 18:39:47 +0200

    add exercises 2.6 - 2.8

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  function handleNameChange(event) {
+    setNewName(event.target.value)
+  }
+
+  function handleNumberChange(event) {
+    setNewNumber(event.target.value)
+  }
+
+  function addPerson(event) {
+    event.preventDefault()
+    let existingPerson = persons.find((element) => element.name === newName)
+
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const person = {
+        name: newName,
+        number: newNumber,
+      }
+      setPersons(persons.concat(person))
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type='submit' onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+export default App
