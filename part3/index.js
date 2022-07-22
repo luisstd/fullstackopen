7610d843560d00dcdd957730746d1735a79bba86
Luis Steidle <luis@steidle.me>
2022-07-22 15:18:25 +0200

    add exercises 3.1 - 3.6

---
+++ part3/index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  let numberPersons = persons.length
+  response.send(`<span>Phonebook has info for ${numberPersons} people</span>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const person = persons.find((person) => {
+    return person.id === id
+  })
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  person.id = createRandomId(500)
+
+  if (person.id && person.name && person.number) {
+    response.json(person)
+  } else {
+    response.status(404).send({ error: 'name and number cannot be undefined' })
+  }
+})
+
+function createRandomId(max) {
+  return Math.floor(Math.random() * max)
+}
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
