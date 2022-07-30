a5a27fbdd93f13af4c3a29a94d55d3980e2b28ab
Luis Steidle <luis@steidle.me>
2022-07-30 16:49:40 +0200

    add frontend build to heroku

--- part3/index.js
+++ part3/index.js
@@ -4,15 +4,14 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(express.json())
-
 morgan.token('body', function (req) {
   return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
