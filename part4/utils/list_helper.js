9fc47ce79ebda84671dac04b9b0853ebc2c3ce90
Luis Steidle <luis@steidle.me>
2022-08-08 18:56:25 +0200

    add exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,10 +9,28 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let i
+
+  // Initialize maximum element
+  let max = blogs[0].likes
+
+  // Traverse array elements
+  // from second and compare
+  // every element with current max
+  for (i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > max)
+      max = blogs[i].likes
+  }
+
+  return blogs.find(blog => blog.likes === max)
+}
+
 
 
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
