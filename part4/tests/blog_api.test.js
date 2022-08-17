ad70c61f952048453e4e0975fa1303745b9a5282
Luis Steidle <luis@steidle.me>
2022-08-17 11:11:27 +0200

    add exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('there are 3 blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(3)
+}, 100000)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
