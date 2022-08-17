f2630e41b1ee7ee3e5c6ea43ad0f505a8fd96e9c
Luis Steidle <luis@steidle.me>
2022-08-17 11:32:21 +0200

    add exercise 4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -17,6 +17,12 @@ test('there are 3 blogs', async () => {
   expect(response.body).toHaveLength(3)
 }, 100000)
 
+test('unique identifier property is called id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+}, 100000)
+
 afterAll(() => {
   mongoose.connection.close()
 })
