6b2d21d88a8dae7b92b0c13c4716e56b7ebc7db4
Luis Steidle <luis@steidle.me>
2022-08-18 17:49:48 +0200

    add exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
+const Blog = require('../models/blog')
+const helper = require('../utils/list_helper')
+
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -11,16 +23,38 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('there are 3 blogs', async () => {
+test('number of all blogs is returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(3)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)
 
 test('unique identifier property is called id', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
+}, 100000)
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Basic Blog Title',
+    author: 'Arthur Author',
+    url: 'https://test.testing',
+    likes: 12,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map((r) => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(contents).toContain('Basic Blog Title')
 }, 100000)
 
 afterAll(() => {
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -19,18 +19,42 @@ const favoriteBlog = (blogs) => {
   // from second and compare
   // every element with current max
   for (i = 1; i < blogs.length; i++) {
-    if (blogs[i].likes > max)
-      max = blogs[i].likes
+    if (blogs[i].likes > max) max = blogs[i].likes
   }
 
-  return blogs.find(blog => blog.likes === max)
+  return blogs.find((blog) => blog.likes === max)
 }
 
-
-
+const initialBlogs = [
+  {
+    title: 'Terrific Testing',
+    author: 'Awesome Author',
+    url: 'https://test.testing',
+    likes: 12,
+  },
+  {
+    title: 'Terrible Testing',
+    author: 'Arrested Author',
+    url: 'https://test.testing',
+    likes: 4,
+  },
+  {
+    title: 'Truthful Testing',
+    author: 'Another Author',
+    url: 'https://test.testing',
+    likes: 45,
+  },
+  {
+    title: 'Trying out Testing',
+    author: 'Alfons Author',
+    url: 'https://test.testing',
+    likes: 23,
+  },
+]
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  initialBlogs,
 }
